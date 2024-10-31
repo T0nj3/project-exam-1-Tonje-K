0cf16fd998bcaa6d041bf9c36a0db8342de1ad0f
T0nj3 <149260628+T0nj3@users.noreply.github.com>
2024-10-31 16:00:23 +0100

    Update all-posts.js

--- JS/all-posts.js
+++ JS/all-posts.js
@@ -34,12 +34,13 @@ function displayPosts() {
     const postElement = document.createElement('div');
     postElement.classList.add('blog-post');
     postElement.innerHTML = `
-    <a href="./one-post.html?id=${post.id}">
-      <img src="${post.media.url}" alt="${post.media.alt}">
-      <h2>${post.title}</h2>
-      <p>${post.shortDescription}</p>
+    postElement.innerHTML = `
+    <a href="../HTML/one-post.html?id=${post.id}">
+        <img src="${post.media.url}" alt="${post.media.alt}">
+        <h2>${post.title}</h2>
+        <p>${post.shortDescription}</p>
     </a>
-  `;
+`;
     blogContainer.appendChild(postElement);
   });
 
